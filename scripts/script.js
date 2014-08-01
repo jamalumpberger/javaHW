1fdfc9b3ca79ab4a22639fda8fe44b65ec78c10c
jamalumpberger <nozaki.kn@gmail.com>
2014-07-31 20:07:00 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+// variable:
+var x = 99999
+
+// alert:
+alert("Don't open this page!")
+// comment:
+// "This is a comment"
+// console:
+console.log("You can see this on the console!")
+
+// data types:
+// a.string
+"What's up?"
+// b. numbers, floats
+var x = 9999
+var y = 9999.1
+// c. boolean
+var x = false
+var y = true
+
+// arrays:
+("Yo", "No", "Woah", "Bro")
+
+//testing (true or false):
+console.log(5-5 == 0) 
+//console returns true
+
+//logic:
+console.log(5 > 2 && 8 < 100000)
+//console returns true
+
+//functions:
+
+function quadruple(x){
+	return x*4
+}
+console.log(quadruple(5))
+
+//Part 2: Three Functions:
+function one(x) {
+	console.log(x**2)
+}
+one(2)
+
+function two(x) {
+	console.log(x*x)
+}
+two(10)
+
+function three(x, y) {
+	alert(x*y)
+}
+three(5, 5)
